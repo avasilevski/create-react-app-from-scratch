825a16db47f945d3a260dbb9dbde26037edbf83a
Aleksandar Vasilevski <vasilevski85@gmail.com>
2020-01-09 22:07:57 +0100

    04-styling-components
    added basic styling

--- src/containers/CryptoCurrencies/CryptoCurrencies.js
+++ src/containers/CryptoCurrencies/CryptoCurrencies.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import Cryptocurrency from '../../components/Cryptocurrency/Cryptocurrency'
+import {Container} from 'react-bootstrap'
 
 class CryptoCurrencies extends Component {
   render(){
     return(
-      <div>
+      <Container>
         <Cryptocurrency name="Bitcoin" symbol="BTC" price="7000"/>
         <Cryptocurrency name="Ethereum" symbol="ETC" price="125"/>
         <Cryptocurrency name="XRP" symbol="XRP" price="0.1"/>
         <Cryptocurrency name="Bitcoin Cash" symbol="BCH" price="180"/>
-      </div>
+      </Container>
     );
   }
 }
